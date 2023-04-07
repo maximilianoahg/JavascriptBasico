a16ecec67b203801f2ffaa80ecdb120ed12615f9
Hernandez2003 <hernandez.gallardo81@gmail.com>
2023-04-07 18:34:11 -0400

    ejercicio n2

---
+++ Ejercicio N2/problema2.js
@@ -0,0 +1,11 @@
+
+const libroFavorito = {
+    titulo: "papelucho",
+    autor: 'Marcela Paz',
+    fecha: '1947',
+    url: 'https://www.google.cl/books/edition/Papelucho/Av4BEAAAQBAJ?hl=es-419&gbpv=1&printsec=frontcover'
+
+}
+
+const datosPersonales = ['Maximiliano', 20, true, libroFavorito];
+console.log(datosPersonales);
