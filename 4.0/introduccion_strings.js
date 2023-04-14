eb2b92ef0023eec15bba8ff81c3c9a5cc14dd4a7
Maximiliano Hernández <hernandez.gallardo81@gmail.com>
2023-04-14 17:55:27 -0400

    4.1 Tipos de declaracion de strings y cuando utilizarlos

---
+++ 4.0/introduccion_strings.js
@@ -0,0 +1,25 @@
+// Sesion 4 - Strings (Cadenas de caracteres)
+
+let str_sgl = 'hola soy un texto con comillas simples';
+let str_dbl = "Hola soy un texto con comillas dobles";
+
+let str_comillas = 'El otro dia me dijo "Ve a sacar la basura"';
+let str_comillas_1 = 'El otro dia me dijo \'Ve a sacar la basura\'';
+let str_comillas_2 = 'El otro dia me dijo "Ve a sacar la basura"';
+
+console.log(str_dbl);
+
+// Comillas inveridas `backticks`
+
+let str_backticks = `Hola esto es un string`;
+
+console.log(str_backticks);
+
+let nombre = 'Maximiliano'
+let saludo = `hola ${nombre}`;
+
+console.log(saludo);
+
+// Introduccion de variables en HTML
+
+let libros = ['Empieza por el por qué', 'El monje que vendio su ferrari', 'El poder de ahora'];
